fdb6475174c13d5a90bd37cb57ce349c7c8ad931
alic3 <alic3dev@gmail.com>
2024-11-19 13:12:45 -0500

    Add Intro module

---
+++ src/Intro.tsx
@@ -0,0 +1,171 @@
+import React from 'react'
+
+import styles from './Intro.module.css'
+
+export function Intro(): React.ReactElement {
+  return (
+    <>
+      <h1 className={styles.header}>As@74Na5</h1>
+      <h2 className={styles.pent}>⛧</h2>
+      <h3 className={styles.num}>715</h3>
+
+      <div>
+        <div className={`${styles['intro-block-container']}`}>
+          <div className={`${styles.blur} ${styles['intro-block']}`}>
+            <div className={styles.intro}>A</div>
+            <div className={styles.intro}>s</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>t</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>n</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>s</div>
+          </div>
+        </div>
+
+        <div className={`${styles['intro-block-container']}`}>
+          <div className={`${styles['intro-block']}`}>
+            <div className={styles.intro}>A</div>
+            <div className={styles.intro}>s</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>t</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>n</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>s</div>
+          </div>
+        </div>
+
+        <div className={`${styles['intro-block-container']}`}>
+          <div className={`${styles.blur} ${styles['intro-block']}`}>
+            <div className={styles.intro}>A</div>
+            <div className={styles.intro}>s</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>t</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>n</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>s</div>
+          </div>
+        </div>
+
+        <div className={`${styles['intro-block-container']}`}>
+          <div className={`${styles['intro-block']}`}>
+            <div className={styles.intro}>A</div>
+            <div className={styles.intro}>s</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>t</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>n</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>s</div>
+          </div>
+        </div>
+
+        <div className={`${styles['intro-block-container']}`}>
+          <div className={`${styles.blur} ${styles['intro-block']}`}>
+            <div className={styles.intro}>A</div>
+            <div className={styles.intro}>s</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>t</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>n</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>s</div>
+          </div>
+        </div>
+
+        <div className={`${styles['intro-block-container']}`}>
+          <div className={`${styles['intro-block']}`}>
+            <div className={styles.intro}>A</div>
+            <div className={styles.intro}>s</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>t</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>n</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>s</div>
+          </div>
+        </div>
+
+        <div className={`${styles['intro-block-container']}`}>
+          <div className={`${styles.blur} ${styles['intro-block']}`}>
+            <div className={styles.intro}>A</div>
+            <div className={styles.intro}>s</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>t</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>n</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>s</div>
+          </div>
+        </div>
+
+        <div className={`${styles['intro-block-container']}`}>
+          <div className={`${styles['intro-block']}`}>
+            <div className={styles.intro}>A</div>
+            <div className={styles.intro}>s</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>t</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>n</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>s</div>
+          </div>
+        </div>
+
+        <div className={`${styles['intro-block-container']}`}>
+          <div className={`${styles.blur} ${styles['intro-block']}`}>
+            <div className={styles.intro}>A</div>
+            <div className={styles.intro}>s</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>t</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>n</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>s</div>
+          </div>
+        </div>
+
+        <div className={`${styles['intro-block-container']}`}>
+          <div className={`${styles['intro-block']}`}>
+            <div className={styles.intro}>A</div>
+            <div className={styles.intro}>s</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>t</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>n</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>s</div>
+          </div>
+        </div>
+
+        <div className={`${styles['intro-block-container']}`}>
+          <div className={`${styles.blur} ${styles['intro-block']}`}>
+            <div className={styles.intro}>A</div>
+            <div className={styles.intro}>s</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>t</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>n</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>s</div>
+          </div>
+        </div>
+
+        <div className={`${styles['intro-block-container']}`}>
+          <div className={`${styles['intro-block']}`}>
+            <div className={styles.intro}>A</div>
+            <div className={styles.intro}>s</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>t</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>n</div>
+            <div className={styles.intro}>a</div>
+            <div className={styles.intro}>s</div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
